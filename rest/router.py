f705c86f5c2066f8ab0868f5df7cc5de20c54cba
guidofenix <guido.barbaglia@fao.org>
2014-07-04 14:57:20 +0200

    FAOSTAT header and footer in Bootstrap

--- rest/router.py
+++ rest/router.py
@@ -1,1 +1,17 @@
-__author__ = 'kalimaha'
+from flask import Flask
+from flask import render_template
+from flask.ext.cors import cross_origin
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@cross_origin(origins='*')
+def root():
+    modis = render_template('modis.html')
+    return render_template('index_bootstrap.html', main_content=modis)
+
+
+if __name__ == '__main__':
+    app.run(port = 5005, debug = True)
